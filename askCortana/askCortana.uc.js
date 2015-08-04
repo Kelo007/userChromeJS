7b6f9d0b6bb3d54ce597b4e9067a6e95ab3e4eda
GH-Kelo <1376312818@qq.com>
2015-08-04 13:38:59 +0800

    2015.08.04 新增召唤小冰

--- askCortana/askCortana.uc.js
+++ askCortana/askCortana.uc.js
@@ -8,8 +8,10 @@
 	var ac = window.askCortana = {
 		// 延时
 		_delay: 100,
-		// 循环次数，解决运行vbs可能未打开开始菜单问题
-		_count: 5,
+		// 循环次数，解决运行vbs可能未打开开始菜单问题 （360或其他杀软问题）
+		_popupmenucount: 5,
+		// Cortana目录（可从开始菜单中拖出），以profile为相对目录。可选
+		_path: "chrome\\Cortana.lnk",
 		get sysVersion() Services.sysinfo.getProperty("version").split(".")[0],
 		init: function() {
 			if (this.sysVersion != 10) {
@@ -21,35 +23,67 @@
 				class: "menu-iconic",
 				label: "询问Cortana",
 				condition: "select",
-				onclick: "askCortana.runCortana(content.getSelection().toString());"
+				onclick: "askCortana.runCortana(content.getSelection().toString())"
+			});
+			menu.appendChild(menuitem);
+			var menuitem = $C("menuitem", {
+				id: "Xiaoice",
+				class: "menu-iconic",
+				label: "召唤小冰",
+				onclick: "askCortana.runXiaoice();"
 			});
 			menu.appendChild(menuitem);
 		},
-		runCortana: function(str) {
-			this.timer && clearTimeout(this.timer);
-			str = str.replace(/[\n\t]/g, "");
+		popupCortanaMenu: function() {
+			var path;
+			if (this._path) {
+				var file = Services.dirsvc.get("ProfD", Ci.nsILocalFile);
+				file.appendRelativePath(this._path);
+				path = file.path;
+			}
+			if (!path) {
+				var vbsText = '\
+					set ws=createobject("wscript.shell")\n\
+					for i=1 to ' + this._popupmenucount + '\n\
+					ws.sendKeys "^{esc}"\n\
+					next\
+				';
+				path = this.createTempFile(vbsText, 'popupMenu.vbs');
+			}
+			this.exec(path, []);
+		},
+		sendKeys: function(keys) {
 			var vbsText = '\
 				set ws=createobject("wscript.shell")\n\
-				for i=1 to ' + this._count + '\n\
-				ws.sendKeys "^{esc}"\n\
-				next\
+				ws.sendKeys "' + keys + '"\
 			';
-			var vbsFile = this.createTempFile(vbsText, 'popupMenu.vbs');
-			this.exec(vbsFile, [], true);
+			var vbsFile = this.createTempFile(vbsText, 'sendKeys.vbs');
+			this.exec(vbsFile, []);
+		},
+		runCortana: function(str, cb) {
+			str = str.replace(/[\n\t]/g, "");
+			this.timer && clearTimeout(this.timer);
+			this.popupCortanaMenu();
 			this.timer = setTimeout(() => {
 				if (document.hasFocus()) {
 					this.runCortana(str);
 					return;
 				}
-				var vbsText = '\
-					set ws=createobject("wscript.shell")\n\
-					code="' + str + '"\n\
-					ws.Run "cmd.exe /c echo " & code & "| clip.exe", vbHide\n\
-					ws.sendKeys "^v"\
-				';
-				var vbsFile = this.createTempFile(vbsText, 'runCortana.vbs');
-				this.exec(vbsFile, []);
+				this.copy(str);
+				this.sendKeys("^v");
+				cb && typeof cb == "function" && cb();
 			}, this._delay);
+		},
+		runXiaoice: function(cb) {
+			this.runCortana("召唤小冰", () => {
+				setTimeout(() => {
+					this.sendKeys("{ENTER}");
+					cb && typeof cb == "function" && cb();
+				}, this._delay * 3)
+			});
+		},
+		copy: function(str) {
+			Cc["@mozilla.org/widget/clipboardhelper;1"].getService(Ci.nsIClipboardHelper).copyString(str);
 		},
 		exec: function(path, args, blocking) {
 			if (typeof blocking == 'undefined') blocking = false;
@@ -66,6 +100,7 @@
 				proc["runw" in proc ? "runw" : "run"](blocking, args, args.length);
 				return true;
 			} else {
+				Cu.reportError('File Not Found: ' + path);
 				return false;
 			}
 		},
